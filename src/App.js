84adecc762c43017d3d830bedf0412af2ce91e3e
fatemeabedi <abedifateme1378@gmail.com>
2023-09-21 14:03:33 +0330

    add header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-import Test from "./components/Test/index.jsx";
-import Test2 from "./components/Test2/index.jsx";
+import Header from "./components/Header/index.jsx";
 
 const App = () => {
   return (
     <>
-      <div>App</div>
-      <img src="./assets/cr7.jpeg" alt="cr7" />
-      <Test />
-      <Test2 />
+      <Header />
     </>
   );
 };
--- src/components/Test2/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./styles.css";
-
-export default function Test2() {
-  return <div>Test2</div>;
-}
