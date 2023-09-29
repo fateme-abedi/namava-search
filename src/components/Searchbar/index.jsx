f7394e1c20835522d2cb5612b44b1e6d5a42b3af
fatemeabedi <abedifateme1378@gmail.com>
2023-09-29 20:44:31 +0330

    add searchbar component

--- src/components/Searchbar/index.jsx
+++ src/components/Searchbar/index.jsx
@@ -4,8 +4,14 @@ import styles from "./style.module.css";
 
 
 export default function SearchBar({setSearchQuery}){
+    const [query, setQuery]=useState('')
     const handleInputChange=(event)=>{
         setSearchQuery(event.target.value);
+        setQuery(event.target.value);
+    }
+    const handleDeleteInput= () => {
+        setQuery('')
+        setSearchQuery('')
     }
 
     return (
@@ -13,7 +19,13 @@ export default function SearchBar({setSearchQuery}){
         <span>
            <img src="./assets/Search.png" alt='search' className={styles.searchIcon} />  
         </span>
-        <input type='text' onChange={handleInputChange}  placeholder="فیلم، سریال، بازیگر و ژانر" />
+        <input type='text' value={query} onChange={handleInputChange}  placeholder="فیلم، سریال، بازیگر و ژانر" />
+        {
+            query?<span onClick={handleDeleteInput}>
+            <img src="./assets/cross.png" alt='delete' className={styles.deleteIcon} /> 
+         </span> : ''
+        }
+        
     </div>
     )
 
