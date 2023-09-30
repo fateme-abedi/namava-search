e002e8073141635563352b5034d026ca96e1d7ac
fatemeabedi <abedifateme1378@gmail.com>
2023-09-30 09:55:47 +0330

    add result component

---
+++ src/components/ResultFrame/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import styles from "./style.module.css";
+import MovieCard from "../MovieCard/index.js";
+
+export default function Container({ movies }) {
+  return (
+    <>
+      <div className={styles.movieList}>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            src={movie.image_url}
+            movieName={movie.name}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
