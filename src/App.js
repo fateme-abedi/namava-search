c4d272589f6e2d4183ed54f62f16dfa50a4c00c1
fatemeabedi <abedifateme1378@gmail.com>
2023-09-30 09:58:28 +0330

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header/index.jsx";
-import Container from "./pages/Container/index.jsx";
+import Header from "./components/Header/index.js";
+import Container from "./components/Container/index.js";
 
 const App = () => {
   return (
