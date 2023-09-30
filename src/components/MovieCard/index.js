5f086f0eef5d6c992482601affd27aa1f5e16004
fatemeabedi <abedifateme1378@gmail.com>
2023-09-30 09:56:13 +0330

    add movie card

---
+++ src/components/MovieCard/index.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import styles from "./style.module.css";
+
+export default function Card({ src, movieName }) {
+  return (
+    <div className={styles.card}>
+      <a href="#" target="_blank">
+        <div className={styles.cardImg}>
+          <img src={src} alt={movieName} />
+        </div>
+        <div className={styles.title}>{movieName}</div>
+      </a>
+    </div>
+  );
+}
